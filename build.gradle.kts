0dbc9a75cf20bf52fa4d3bdedb7f44a544a96bee
doomsdayrs <doomsdayrs@gmail.com>
2022-04-22 13:28:04 -0400

    Clean out extra stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.dokka.gradle.DokkaTask
-
 buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
@@ -13,9 +11,6 @@ repositories {
 }
 
 plugins {
-    // Dev Plugins
-    id("idea")
-
     // Base Plugins
     kotlin("multiplatform") version "1.6.20"
     id("com.android.library") version "7.1.3"
@@ -23,18 +18,12 @@ plugins {
     // Publishing Plugins
     signing
     `maven-publish`
-    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-
-    // Docs Plugins
-    id("org.jetbrains.dokka") version "1.6.20"
 }
 
 group = "io.github.g0dkar"
-version = "3.1.0b"
+version = "3.1.1b"
 
 kotlin {
-
-
     android {
         publishLibraryVariants("release")
 
@@ -67,145 +56,5 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
-/* **************** */
-/* Dev Environment  */
-/* **************** */
-idea {
-    module {
-        isDownloadJavadoc = false
-        isDownloadSources = true
-    }
-}
-
-/* **************** */
-/* Docs             */
-/* **************** */
-val dokkaOutputDir = "$projectDir/dokka"
-
-tasks.getByName<DokkaTask>("dokkaHtml") {
-    outputDirectory.set(file(dokkaOutputDir))
-}
-
-val javadocJar = tasks.register<Jar>("javadocJar") {
-    dependsOn(tasks.dokkaHtml)
-    archiveClassifier.set("javadoc")
-    from(dokkaOutputDir)
-}
-
-/* **************** */
-/* Lint             */
-/* **************** */
-val ktlint by configurations.creating
-
-dependencies {
-    ktlint("com.pinterest:ktlint:0.45.1") {
-        attributes {
-            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
-        }
-    }
-}
-
-val ktlintCheck by tasks.creating(JavaExec::class) {
-    description = "Check Kotlin code style."
-    mainClass.set("com.pinterest.ktlint.Main")
-    classpath = ktlint
-    args = listOf("--editorconfig=$projectDir/.editorconfig", "--color", "--relative", "src/**/*.kt")
-}
-
-val ktlintFormat by tasks.creating(JavaExec::class) {
-    description = "Fix Kotlin code style deviations."
-    mainClass.set("com.pinterest.ktlint.Main")
-    classpath = ktlint
-    args = listOf("--editorconfig=$projectDir/.editorconfig", "--format", "--color", "--relative", "src/**/*.kt")
-}
-
-tasks {
-    publish {
-        dependsOn(ktlintCheck)
-    }
-
-    publishToMavenLocal {
-        dependsOn(ktlintCheck)
-    }
-}
-
-/* **************** */
-/* Publishing       */
-/* **************** */
-val ossrhUsername = properties.getOrDefault("ossrhUsername", System.getenv("OSSRH_USER"))?.toString()
-val ossrhPassword = properties.getOrDefault("ossrhPassword", System.getenv("OSSRH_PASSWORD"))?.toString()
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-
-            username.set(ossrhUsername ?: return@sonatype)
-            password.set(ossrhPassword ?: return@sonatype)
-        }
-    }
-}
-
-signing {
-    val key = properties.getOrDefault("signingKey", System.getenv("SIGNING_KEY"))?.toString() ?: return@signing
-    val password =
-        properties.getOrDefault("signingPassword", System.getenv("SIGNING_PASSWORD"))?.toString() ?: return@signing
-
-    useInMemoryPgpKeys(key, password)
-    sign(publishing.publications)
-}
-
-publishing {
-    publications {
-        withType<MavenPublication> {
-            artifact(javadocJar)
-
-            pom {
-                val projectGitUrl = "https://github.com/g0dkar/qrcode-kotlin"
-
-                name.set(rootProject.name)
-                description.set("A Kotlin Library to generate QR Codes without any other dependencies.")
-                url.set(projectGitUrl)
-                inceptionYear.set("2021")
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("$projectGitUrl/blob/main/LICENSE")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("g0dkar")
-                        name.set("Rafael Lins")
-                        email.set("rafael@lins.net.br")
-                        url.set("https://github.com/g0dkar")
-                    }
-                }
-                issueManagement {
-                    system.set("GitHub")
-                    url.set("$projectGitUrl/issues")
-                }
-                scm {
-                    connection.set("scm:git:$projectGitUrl")
-                    developerConnection.set("scm:git:$projectGitUrl")
-                    url.set(projectGitUrl)
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            name = "sonatype"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = ossrhUsername
-                password = ossrhPassword
-            }
-        }
     }
 }
